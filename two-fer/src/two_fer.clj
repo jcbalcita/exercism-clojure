902e84b4f4887e3d20f69f53a0233068a4578548
John Carlo Aspiras Balcita <john.balcita@gmail.com>
2018-11-04 14:27:48 -0800

    str takes more than one arg

--- two-fer/src/two_fer.clj
+++ two-fer/src/two_fer.clj
@@ -1,9 +1,8 @@
 (ns two-fer)
 
 (defn make-message [name]
-  (str (str "One for " name) ", one for me."))
+  (str "One for " name ", one for me."))
 
 (defn two-fer 
   ([] (make-message "you")) 
-  ([name] (make-message name))
-)
+  ([name] (make-message name)))
